32f544302ef540801fe99dcf15418f295d19be62
unknown <fernojo99@gmail.com>
2023-02-28 22:52:00 -0600

    primer commit

---
+++ menu.py
@@ -0,0 +1,31 @@
+
+n = int(0)
+
+while (n!=5):
+    print("     --Menu--")
+    print("1. carnet")
+    print("2. Nombre")
+    print("3. Frase")
+    print("4. Opinion")
+    print("5. Finalizar")
+    m = input("Ingresa un numero:")
+
+    m = int(m)
+
+    if m == 1:
+        print("201903922")
+    if m==2:
+        print("Fernando Jose Lopez Doño")
+    if m==3:
+        print("No importa que tan difícil o imposible sea, no le pierdas la vista a tu objetivo.")
+        print("     -Monkey D. Luffy")
+    if m==4:
+        print("Ciertamente es una carrera dificil, requiere de mucho esfuerzo y tiempo")
+        print("hay demasiado por aprender y tan solo elegir una ruta a seguir como desarrollador ")
+        print("ya es bastante dificil, y no solo eso si no que cada dia se abren mas ramas de")
+        print("desarrollo de software, en parte hace dificil saber que hacer, pero a su vez")
+        print("da infinidad de posibilidades a los programadores del futuro y definitivamente quiero estar ahi.")
+    if m==5:
+        exit
+        break 
+    
---
+++ variables.py
@@ -0,0 +1,6 @@
+edad = int(22)
+pi = float(3.1416)
+nombre =  str("Darlin")
+soltera = True
+hobbies = ["tomarse fotos", "ver pelis", "tiktok"]
+
